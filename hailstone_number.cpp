876f97ad651a1b809c2409731620f387bdfadeeb
vivek mandavara <vivekmandavara1029@gmail.com>
2026-08-13 20:12:11 +0530

    print the series of hailstone number .
    for example : you pick any positive whole number to start the series.
    now,
    if the number is even, you divides its by two. if it is odd, you multiply by 3 and add one (3*n+1) until the last digit of series is one.

---
+++ hailstone_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+int main(){
+	int  n;
+	cout<<"enter the value of n = ";
+	cin>>n;
+	while(n!=1){
+	if(n%2==0)
+	n=n/2;
+	else
+	n=((3*n)+1);
+	cout<<n<<endl;}
+	return 0;}
