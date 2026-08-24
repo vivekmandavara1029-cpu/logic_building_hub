d8df6457be7f9073db5daa35c46ed755b6305dae
vivek mandavara <vivekmandavara1029@gmail.com>
2026-08-24 22:29:02 +0530

    print the equilateral triangle using pattern printing concept.
    for example 
    the given input : n = 3
    now, the output is     
        *
      ***
     *****

---
+++ pattern_equilateraltriangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+int n,i,j,k,copy;
+cout<<"enter no.";
+cin>>n;
+for(i=1;i<=n;i++){
+for(j=1;j<=(n-i);j++){
+	 cout<<" ";}
+	 for(k=1;k<=(2*i-1);k++){
+	cout<<"*";}
+	cout<<endl;}
+	return 0;}
+	
