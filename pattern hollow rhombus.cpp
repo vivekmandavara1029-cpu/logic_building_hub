a42fdd851746b1e84653754c9dae5b78ba4d4d8a
vivek mandavara <vivekmandavara1029@gmail.com>
2026-08-25 17:49:30 +0530

    print the hollow rhombus using pattern printing concept.
    for example :  the given input n = 3
    the output is 
          *
        ***
      *****
        ***
         *

---
+++ pattern hollow rhombus.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main (){
+int n,i,j,v;
+cout<<"enter no.";
+cin>>n;
+v=(2*n-3);
+for(i=1;i<=n;i++){
+for(j=1;j<=(n-i);j++){
+	 cout<<" ";}
+	 for(j=1;j<=(2*i-1);j++){
+	 	if((j==1)||(j==(2*i-1)))
+	cout<<"*";
+	else
+	cout<<" ";}
+	cout<<endl;}
+for(i=2;i<=n;i++){
+for(j=1;j<=(i-1);j++){
+	 cout<<" ";}
+	 for(j=v;j>0;j--){
+	 	if((j==v)||(j==1))
+	cout<<"*";
+	else
+	cout<<" ";}
+v=v-2;
+	cout<<endl;}
+	return 0;}
+	
